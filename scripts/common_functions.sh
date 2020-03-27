66c2f1eb93d508cccd427bf19ae521aebc7a557a
adolfogustavoca <adolfogustavoca@gmail.com>
2020-03-27 18:39:52 -0500

    Update common_functions.sh

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -14,7 +14,7 @@
 # permissions and limitations under the License.
 
 # ELB_LIST defines which Elastic Load Balancers this instance should be part of.
-ELB_LIST="PRD_LATINA_WFE"
+ELB_LIST="QAS_LATINA_WFE"
 
 # Under normal circumstances, you shouldn't need to change anything below this line.
 # -----------------------------------------------------------------------------
